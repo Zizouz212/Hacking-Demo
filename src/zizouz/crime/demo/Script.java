d82d7fe95db63f5e765c5b8689b0ae418e24f450
Zizouz212 <Zizouz212@users.noreply.github.com>
2016-01-12 15:19:45 -0500

    Update Script.java

--- src/zizouz/crime/demo/Script.java
+++ src/zizouz/crime/demo/Script.java
@@ -3,54 +3,48 @@ package zizouz.crime.demo;
 public class Script 
 {
 	
-	public static final String[] script = new String[] {
+	public static final String[][] script = new String[][] {
 		
-		"Stop. Look. Listen. I'm not actually a video.",
-		"I'm not 'QuickTime'. I'm something else",
-		"So then, what am I?",
+		{"Stop. Look. Listen. I'm not actually a video.", },
+		{"I'm not 'QuickTime'. I'm something else", },
+		{"So then, what am I?", },
 		
-		"I am malicious. I am a virus. I am spyware. I am malware.",
-		"Call me anything.",
-		"But remember. I still look like a video.",
-		"So what can I do?",
+		{"I am malicious. I am a virus. I am spyware. I am malware.", }
+		{"Call me anything.", },
+		{"But remember. I still look like a video.", },
+		{"So what can I do?", },
 		
-		"I can...",
+		{"I can...", },
 			
-			"Find your name. Hello <>",
-			"Oh, I didn't mention I could access the system voice?",
-			"I know your operating system. You have a <> machine? Cool.",
-			"Let me get your password. Your password is... I'm not saying that",
+			{"Find your name. Hello <>", },
+			{"Oh, I didn't mention I could access the system voice?", },
+			{"I know your operating system. You have a <> machine? Cool.", },
+			{"Let me get your password. Your password is... I'm not saying that", },
 			
-		"What else can I do, you ask?",
+		{"What else can I do, you ask?", },
 			
-			"I can read all. Of your files",
-			"I can make thousands of files",
-			"I can delete them, change them, modify them",
-			"I can even delete your entire computer",
+			{"I can read all. Of your files", },
+			{"I can make thousands of files", },
+			{"I can delete them, change them, modify them", },
+			{"I can even delete your entire computer", },
 			
-		"Let's see...",
+		{"Let's see...", },
 		
-			"You've got <>, <>, <> installed?",
-			"Here's a document I found... <>",
+			{"You've got <>, <>, and <> installed?", },
+			{"Here's a document I found... <>", },
 			
-		"I forgot, I'm still full screen! Let me show you what I have done",
+		{"I forgot, I'm still full screen! Let me show you what I have done", },
 		
 		/*
 			java.awt.Desktop().getDesktop().browse(directory);
 		*/
 		
 		
-		"Step up to cybercrime. Don't let us do this",
+		{"Step up to cybercrime. Don't let us do this", },
 		
-		"In 2012, the RCMP received around 4000 incidents of cybercrime. 85% was where technology was ued as an instrument to commit another crime, such as fraud, identity theft, to more serious crimes, such as human and drug trafficking, and child sexual exploitation.",
+		{"In 2012, the RCMP received around 4000 incidents of cybercrime. 85% was where technology was ued as an instrument to commit another crime, such as fraud, identity theft, to more serious crimes, such as human and drug trafficking, and child sexual exploitation.", },
 		
 	};
-
-
-
-
-
-
 
 
 
